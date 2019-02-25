5ad93df3ee00f6623dcab89fc5b3b0ef32acf8cf
Dxcuer <thanapatkjm@hotmail.com>
2019-02-25 15:07:33 +0700

    Add Error Detector and Add Search

--- webReview/urls.py
+++ webReview/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('creating/', views.creating, name='creating'),
     path('res/<int:rest_id>/', views.res, name='res'),
     path('add_review/<int:rest_id>', views.add_review, name='add_review'),
+    path('home/searching', views.searching, name='searching'),
 ]
     # {% for name in latest_animal_name %}
     #     <li><a href="/animalSite/{{ Animal.id }}/">{{ name.animal_name }}</a></li>
--- webReview/views.py
+++ webReview/views.py
@@ -14,29 +14,46 @@ def add_rest(request):
     return render(request, 'add_rest.html')
 
 def creating(request):
-    add = Restaurant(name=request.POST['Rname'],description=request.POST['Rdes'],address=request.POST['Radd'],phone_num=request.POST['Rphone'],Category=request.POST['Rcate'])
-    add.save()
-    return HttpResponseRedirect(reverse('review:home'))
+    if(request.POST['Rname'] and request.POST['Rdes'] and request.POST['Radd'] and request.POST['Rphone'] != ""):
+        add = Restaurant(name=request.POST['Rname'],description=request.POST['Rdes'],address=request.POST['Radd'],phone_num=request.POST['Rphone'],Category=request.POST['Rcate'])
+        add.save()
+        return HttpResponseRedirect(reverse('review:home'))
+    else:
+        return render(request,'add_rest.html',{'error':"You need to fill all Boxes Below"})
 
 def res(request,rest_id):
     rest_descrip = get_object_or_404(Restaurant, pk=rest_id)
-    # try:
     rev =  Reviews.objects.filter(restau=rest_id)
-# except:
-    # print('aaaaaaaaaaaaa')
-    # return render(request, 'rest.html',{'rest_descrip':rest_descrip})
-# else:
     return render(request, 'rest.html',{'rest_descrip':rest_descrip,'rev':rev})
-    # return render(request, 'rest.html',{'rest_descrip':rest_descrip})
 
 def add_review(request,rest_id):
-    rest = Restaurant.objects.get(id=rest_id)
-    add = Reviews(name=request.POST['Rname'],comment=request.POST['Rcomm'],rating=request.POST['Rating'],restau=rest)
-    add.save()
-    fil = Reviews.objects.filter(restau=rest_id)
-    rate = 0
-    for i in range (Reviews.objects.filter(restau=rest_id,).count()) :
-        rate = rate + fil[i].rating
-    rest.rating=rate/Reviews.objects.filter(restau=rest_id,).count()
-    rest.save()
-    return HttpResponseRedirect(reverse('review:res', args=(rest_id,)))
+    try:
+        if(request.POST['Rname'] and request.POST['Rcomm'] and request.POST['Rating'] != ""):
+            rest = Restaurant.objects.get(id=rest_id)
+            add = Reviews(name=request.POST['Rname'],comment=request.POST['Rcomm'],rating=float(request.POST['Rating']),restau=rest)
+            add.save()
+            fil = Reviews.objects.filter(restau=rest_id)
+            rate = 0
+            for i in range (Reviews.objects.filter(restau=rest_id,).count()) :
+                rate = rate + fil[i].rating
+            rest.rating=rate/Reviews.objects.filter(restau=rest_id,).count()
+            rest.save()
+            return HttpResponseRedirect(reverse('review:res', args=(rest_id,)))
+        else:
+            rest_descrip = get_object_or_404(Restaurant, pk=rest_id)
+            rev =  Reviews.objects.filter(restau=rest_id)
+            return render(request, 'rest.html',{'rest_descrip':rest_descrip,'rev':rev,'error':"** You must fill all the data **"})
+    except:
+        rest_descrip = get_object_or_404(Restaurant, pk=rest_id)
+        rev =  Reviews.objects.filter(restau=rest_id)
+        return render(request, 'rest.html',{'rest_descrip':rest_descrip,'rev':rev,'error':"** You must Choose Rating **"})
+
+def searching(request):
+    if(request.POST['searchby']=='name'):
+        list_rest = Restaurant.objects.filter(name__contains=request.POST['search'])
+    else:
+        if(request.POST['Rcate']==""):
+            list_rest = Restaurant.objects.order_by("name")
+        else:
+            list_rest = Restaurant.objects.filter(Category=request.POST['Rcate'])
+    return render(request,'home.html',{'list_rest':list_rest})
